71eb2833985168299ec90ba26e52f2e014f6c3dc
Yuan <q210948213@gmail.com>
2022-10-28 11:37:52 +0800

    test_1

--- Scrips/Cuttlefish/ktx.js
+++ Scrips/Cuttlefish/ktx.js
@@ -15,7 +15,7 @@
 [rewrite_local]
 
 # ～ Vista看天下锁会员权限（2022-08-31）@ddgksf2013
-^https?:\/\/open3\.vistastory\.com\/v\d\/api\/(article\/article_detail|vip\/get_vip|my\/home\/get_home_center) url script-response-body https://raw.githubusercontent.com/Yuanxsxs/QtumultX/master/Scrips/Cuttlefish/ktx.js
+^https?:\/\/open3\.vistastory\.com\/v\d\/api\/(article\/article_detail|vip\/get_vip|my\/home\/get_home_center) url script-response-body ktx.js
 
 [mitm]
 
