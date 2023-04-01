62a6f1dfb4f04bdaf84122e76938399a34c4ff38
Yuan <q210948213@gmail.com>
2023-04-02 00:15:37 +0800

    新增解析非raw链接的 gitthub.com 链接

--- tools/make_quick_installation_link.py
+++ tools/make_quick_installation_link.py
@@ -6,7 +6,7 @@ from urllib.parse import quote
 def Output(raw,tag = "Yuan's Selfuse Rewrite"):
     '''raw = https://raw.githubusercontent.com/Yuanxsxs/QtumultX/master/Rewrite/Crack/Ego_reader.conf'''
     
-    set = re.search("^https?:\/\/raw\.githubusercontent\.com\/(?P<author>.*?)\/.*\/(?P<tag>.*)\.[(conf)|(snippet)|(txt)(json)]",raw)
+    set = re.search("^https?:\/\/raw\.githubusercontent\.com\/(?P<author>.*?)\/.*\/(?P<tag>.*)\.(conf|snippet|txt|json|js)$",raw)
     tag = set.group('tag') + '-' + set.group("author")
     txt = f'''{{"rewrite_remote":["{raw}?raw=true,tag={tag}"]}}'''
     cont_enquoted = quote(txt,encoding='utf-8')
@@ -15,11 +15,21 @@ def Output(raw,tag = "Yuan's Selfuse Rewrite"):
     return url
 def main():
     raw = pyperclip.paste()
-    research = re.search("^https?:\/\/raw\.githubusercontent\.com\/(?P<author>.*?)\/.*\/(?P<tag>.*)\.[(conf)|(snippet)|(txt)(json)]",raw)
+    research = re.search("^https?:\/\/raw\.githubusercontent\.com\/(?P<author>.*?)\/.*\/(?P<tag>.*)\.(conf|snippet|txt|json|js)$",raw)
+    # print(research)
     if research == None:
-        raw = input('请复制正确的raw链接并输入在下方:\n')
+        '''https://github.com/Yuanxsxs/QtumultX/blob/master/Rewrite/AD_block/Cuttfish/StartUp.conf'''
+        url = re.search('^https?:\/\/github\.com/(?P<author>.*?)\/.*\/blob\/.*\.(conf|snippet|txt|json|js)$',raw).group()
+        
+        if url ==  None:
+            raw = input('请复制正确的raw链接并输入在下方:\n')
+        else:
+            print('虽然不是正确的raw,但问题不大.')
+            url = re.sub("github.com","raw.githubusercontent.com",url)
+            raw = re.sub("\/blob","",url)
+        # print(raw)        
     output = Output(raw)
     pyperclip.copy(output)
-    print(f'{output}\n已复制!')
+    print(f'一键安装链接:\n{output}\n已复制!')
 if __name__ == '__main__':
     main()
