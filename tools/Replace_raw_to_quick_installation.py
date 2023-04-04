0c13f6a2170b2b7cefa8ef0851b483c676a36cfe
Yuan <q210948213@gmail.com>
2023-04-04 15:53:25 +0800

    优化代码结构

--- tools/Replace_raw_to_quick_installation.py
+++ tools/Replace_raw_to_quick_installation.py
@@ -4,34 +4,6 @@ from urllib.parse import quote
 '''
 用来把readme文件中的raw值 替换为 一键导入qx链接
 '''
-def Output(url,tag = "Yuan's Selfuse Rewrite"):
-    '''raw = https://raw.githubusercontent.com/Yuanxsxs/QtumultX/master/Rewrite/Crack/Ego_reader.conf'''
-    
-
-    rawre = re.search("^https?:\/\/(raw|gist)\.githubusercontent\.com\/(?P<author>.*?)\/.*\/(?P<tag>.*)\.(?P<suffix>(conf|snippet|txt|json|js|list|qxrewrite)).*",url)   
-    if rawre == None:#条件成立时 该链接不是raw 不成立时为raw
-        urlre = re.search('^https?:\/\/github\.com/(?P<author>.*?)\/.*\/(blob|raw)\/.*\.(conf|snippet|txt|json|js|list|qxrewrite).*',url)
-        if urlre ==  None:
-            print(f"无法识别该{url}")
-            exit()
-        else:
-            url = urlre.group()
-            url = re.sub("github.com","raw.githubusercontent.com",url)
-            url = re.sub("\/(blob|raw)\/","\/",url)
-    rawre = re.search("^https?:\/\/(raw|gist)\.githubusercontent\.com\/(?P<author>.*?)\/.*\/(?P<tag>.*)\.(?P<suffix>(conf|snippet|txt|json|js|list|qxrewrite)).*",url)
-    raw = url
-    tag = rawre.group('tag') + '-' + rawre.group("author")#显示标签
-    suffix =  rawre.group('suffix')#选出后缀来决定添加的资源
-    if suffix in ['json'] :#还要分是task 还是 icon 但是现在暂时只考虑icon
-        txt = f'''["{raw}"]'''
-        quick_install_url = "https://quantumult.app/x/open-app/ui?module=gallery&type=icon&action=add&content=" + quote(txt,encoding='utf-8')
-    else :
-        if suffix in ['conf','snippet','qxrewrite','js']:
-            txt = f'''{{"rewrite_remote":["{raw}?raw=true,tag={tag}"]}}'''
-        if suffix in ['txt','list']:
-            txt = f'''{{"filter_remote":["{raw}?raw=true,tag={tag}"]}}'''
-        quick_install_url = "https://quantumult.app/x/open-app/add-resource?remote-resource=" + quote(txt,encoding='utf-8')
-    return quick_install_url
 def subFunc(search):
     f = search.group()
     n = Output(f)
--- tools/make_quick_installation_link.py
+++ tools/make_quick_installation_link.py
@@ -3,11 +3,23 @@ from urllib.parse import quote
 ''' pyperclip 为第三方库 使用 pip install pyperclip 进行安装
     
 '''
+pattern_raw = "^https?:\/\/(raw\.githubusercontent|gist\.githubusercontent|gitlab)\.com\/(?P<author>.*?)\/.*\/(?P<tag>.*)\.(?P<suffix>(conf|snippet|txt|json|js|list|qxrewrite))$"
+pattern_like_raw ='^https?:\/\/github\.com/(?P<author>.*?)\/.*\/(blob|raw)\/.*\.(conf|snippet|txt|json|js|list|qxrewrite).*'#可转化为raw的似raw非rawurl
 def Output(raw,tag = "Yuan's Selfuse Rewrite"):
-    '''raw = https://raw.githubusercontent.com/Yuanxsxs/QtumultX/master/Rewrite/Crack/Ego_reader.conf'''
+    '''raw = https://raw.githubusercontent.com/Yuanxsxs/QtumultX/master/Rewrite/Crack/Ego_reader.conf
+       raw = https://gitlab.com/lodepuly/vpn_tool/-/raw/main/Tool/Loon/Rule/OpenAI.list
+    '''
     
-    resarch = re.search("^https?:\/\/(raw\.githubusercontent|gitlab)\.com\/(?P<author>.*?)\/.*\/(?P<tag>.*)\.(?P<suffix>(conf|snippet|txt|json|js|list|qxrewrite))$",raw)#必须是raw
-    
+    resarch = re.search(pattern_raw,raw)#必须是raw
+    if resarch == None:#当输入的不是raw时
+        resarch = re.search(pattern_like_raw,raw)#判断是不是类raw链接
+        if resarch == None:#当url为非 类raw链接时
+            print('无法识别此url!')
+        else: #当url为类raw链接时 转化为raw链接
+            url = re.sub("github.com","raw.githubusercontent.com",raw)
+            raw = re.sub("\/(blob|raw)\/","\/",url)
+            
+    resarch = re.search(pattern_raw,raw)
     tag = resarch.group('tag') + '-' + resarch.group("author")#显示标签
     suffix =  resarch.group('suffix')#选出后缀来决定添加的资源
     if suffix in ['json'] :#还要分是task 还是 icon 但是现在暂时只考虑icon
@@ -17,24 +29,11 @@ def Output(raw,tag = "Yuan's Selfuse Rewrite"):
         if suffix in ['conf','snippet']:
             txt = f'''{{"rewrite_remote":["{raw}?raw=true,tag={tag}"]}}'''
         if suffix in ['txt','list']:
-            txt = f'''{{"filter_remote":["{raw}?raw=true,tag={tag}"]}}'''
+            txt = f'''{{"filter_remote":["{raw}?raw=true,tag={tag},force-policy={resarch.group('tag')}"]}}'''
         url = "https://quantumult.app/x/open-app/add-resource?remote-resource=" + quote(txt,encoding='utf-8')
     return url
 def main():
-    raw = pyperclip.paste()
-    research = re.search("^https?:\/\/(raw\.githubusercontent|gitlab)\.com\/(?P<author>.*?)\/.*\/(?P<tag>.*)\.(conf|snippet|txt|json|js|list|qxrewrite)$",raw)#必须是raw
-    # print(research)
-    if research == None:
-        '''https://github.com/Yuanxsxs/QtumultX/(blob|raw)/master/Rewrite/AD_block/Cuttfish/StartUp.conf'''
-        url = re.search('^https?:\/\/github\.com/(?P<author>.*?)\/.*\/(blob|raw)\/.*\.(conf|snippet|txt|json|js|list|qxrewrite)$',raw)#可转化为raw的url
-        if url ==  None:
-            raw = input('请复制正确的raw链接并输入在下方:\n')
-        else:
-            url = url.group()
-            print('虽然不是正确的raw,但问题不大.')
-            url = re.sub("github.com","raw.githubusercontent.com",url)
-            raw = re.sub("\/(blob|raw)\/","\/",url)
-        # print(raw)        
+    raw = pyperclip.paste()       
     output = Output(raw)
     pyperclip.copy(output)
     print(f'一键安装链接:\n{output}\n已复制!两秒后自动关闭')
